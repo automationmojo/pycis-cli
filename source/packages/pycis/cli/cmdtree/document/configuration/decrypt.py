a4f2ef0ce79dc5354f533b24f77e90c2c99a6918
Myron Walker <myron.walker@gmail.com>
2023-09-04 21:48:23 -0500

    Some updated for the configuration encryption and decryption

--- source/packages/pycis/cli/cmdtree/document/configuration/decrypt.py
+++ source/packages/pycis/cli/cmdtree/document/configuration/decrypt.py
@@ -9,6 +9,7 @@ __status__ = "Development" # Prototype, Development or Production
 __license__ = "MIT"
 
 import json
+import os
 
 import click
 
@@ -19,12 +20,17 @@ HELP_PASSPHRASE = "A passphrase to use to decrypt the file."
 @click.command("decrypt")
 @click.option("--document", required=True, type=click.Path(exists=True, file_okay=True, dir_okay=False), help=HELP_DOCUMENT)
 @click.option("--passphrase", required=True, help=HELP_PASSPHRASE)
-@click.argument('filename', metavar='<testrun document>', type=click.Path(dir_okay=False))
+@click.argument('filename', metavar='<plain-document>', type=click.Path(dir_okay=False))
 def command_pycis_document_configuration_decrypt(document: str, passphrase: str, filename: str):
 
+    from mojo.config.configurationformat import ConfigurationFormat
     from mojo.config.cryptography import (
         decrypt_content, generate_fernet_key
     )
+
+    document = os.path.abspath(os.path.expandvars(os.path.expanduser(document)))
+    filename = os.path.abspath(os.path.expandvars(os.path.expanduser(filename)))
+    filebase, _ = os.path.splitext(filename)
 
     key = generate_fernet_key(passphrase)
 
@@ -37,11 +43,23 @@ def command_pycis_document_configuration_decrypt(document: str, passphrase: str,
         plain_config = None
         if "encrypted_content" in encrypted_config:
             cipher_config = encrypted_config["encrypted_content"]
+            format = encrypted_config["format"]
             plain_config = decrypt_content(key, cipher_config)
+
+            ext = None
+            if format == ConfigurationFormat.JSON:
+                ext = ".json"
+            elif format == ConfigurationFormat.YAML:
+                ext = ".yaml"
+            else:
+                errmsg = "Unknown encrypted configuration format '{format}'."
+                raise click.BadParameter(errmsg)
+
+            filename = filebase + ext
         else:
             plain_config = encrypted_config
 
         with open(filename, 'w+') as pcf:
-            json.dump(plain_config, pcf, indent=4)
+            pcf.write(plain_config)
 
     return
--- source/packages/pycis/cli/cmdtree/document/configuration/encrypt.py
+++ source/packages/pycis/cli/cmdtree/document/configuration/encrypt.py
@@ -9,6 +9,7 @@ __status__ = "Development" # Prototype, Development or Production
 __license__ = "MIT"
 
 import json
+import os
 
 import click
 
@@ -18,12 +19,26 @@ HELP_PASSPHRASE = "A passphrase to use to encrypt the file."
 @click.command("encrypt")
 @click.option("--document", required=True, type=click.Path(exists=True, file_okay=True, dir_okay=False), help=HELP_DOCUMENT)
 @click.option("--passphrase", required=True, help=HELP_PASSPHRASE)
-@click.argument('filename', metavar='<testrun document>', type=click.Path(dir_okay=False))
+@click.argument('filename', metavar='<encrypted-document>', type=click.Path(dir_okay=False))
 def command_pycis_document_configuration_encrypt(document: str, passphrase: str, filename: str):
 
+    from mojo.config.configurationformat import ConfigurationFormat
     from mojo.config.cryptography import (
         create_encrypted_configuration, generate_fernet_key
     )
+
+    document = os.path.abspath(os.path.expandvars(os.path.expanduser(document)))
+    filename = os.path.abspath(os.path.expandvars(os.path.expanduser(filename)))
+
+    format = None
+    _, ext = os.path.splitext(document)
+    if ext in [".yml", ".yaml"]:
+        format = ConfigurationFormat.YAML
+    elif ext == ".json":
+        format = ConfigurationFormat.JSON
+    else:
+        errmsg = f"Un-Support file format '{ext}'."
+        click.BadParameter(errmsg)
 
     key = generate_fernet_key(passphrase)
 
@@ -31,7 +46,7 @@ def command_pycis_document_configuration_encrypt(document: str, passphrase: str,
 
     with open(document, 'r') as cf:
         ccontent = cf.read()
-        encrypted_config = create_encrypted_configuration(key, ccontent)
+        encrypted_config = create_encrypted_configuration(key, ccontent, format=format)
 
     if encrypted_config is not None:
         with open(filename, 'w+') as ecf:
